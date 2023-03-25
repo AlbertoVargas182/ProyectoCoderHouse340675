e44d922bec230dedacfe1b2dcaefcc8b1b980384
AlbertoVargas182 <alberto.vargas2104@gmail.com>
2023-03-25 19:28:13 -0300

    segundo commit estable y adicion de formulario

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from AppCoder.views import cursos, estudiantes, profesores, crear_curso
 
 urlpatterns = [
-    path('cursos', cursos),
-    path('curso/<nombre>/<camda>', crear_curso),
-    path('estudiantes', estudiantes),
-    path('profesores', profesores ),
+    path('cursos', cursos, name="AppCoderCursos"),
+    path('buscar_curso', busqueda_curso, name="AppCoderBuscarCurso"),
+    path('curso/<nombre>/<camda>', crear_curso, name="AppCoderCurso"),
+    path('estudiantes', estudiantes, name="AppCoderEstudiantes"),
+    path('profesores', profesores, name="AppCoderProfesores"),
 ]
 
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,21 +1,56 @@
 from multiprocessing import context
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from AppCoder.forms import CursoForm, BusquedaCursoForm
 from AppCoder.models import Curso, Estudiantes, Profesor, Entregable
 
 
-# Create your views here.
+
 '''
 def guardar_curso(request, camada):
     curso = Curso(nombre="Python", camada=camada)
     curso.save()
     return HttpResponse("Usuario creado exitosamente")
 '''
+def busqueda_curso(request):
+    # Mostrar los datos filtrados
+    mi_formulario = BusquedaCursoForm(request.GET)
+    if mi_formulario.is_valid():
+        informacion = mi_formulario.cleaned_data
+        cursos_filtrados = Curso.objects.filter(nombre__icontains=informacion['nombre'])
+        context = {
+            "cursos": cursos_filtrados,
+        }
+    
+        return render(request, "AppCoder/busqueda_curso.html", context=context)
+
+
+
+
+
+
+
 # Ver los cursos guardados
 def cursos(request):
+
+    if request.method == "POST":
+        mi_formulario = CursoForm(request.POST)
+
+        if mi_formulario.is_valid():
+            informacion = mi_formulario.cleaned_data
+            curso_save = Curso(
+                nombre=informacion['nombre'],
+                camada=informacion['camada']
+            )
+            curso_save.save()
+        
+
+
     all_cursos = Curso.objects.all()
     context = {
-        "cursos": all_cursos
+        "cursos": all_cursos,
+        "form": CursoForm(),
+        "form_busqueda": BusquedaCursoForm(),
     }
     return render(request, "AppCoder/cursos.html", context=context)
 
